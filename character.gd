1ed86478d1b331b29a511d034273b6de61f25623
Ferran Marlet <ferran.marlet@holaluz.com>
2020-07-19 17:59:31 +0200

    Added jumping.

--- character.gd
+++ character.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
 export (int, 0, 400) var speed = 200
+
+const GRAVITY = 10
+const JUMPING_POWER = 500
+
 var state = "idle"
-var facingLeft = false;
+var facingLeft = false
+var isOnFloor = true
+var inertia = 0
 
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
@@ -22,26 +28,31 @@ func _process(delta):
 			facingLeft = false
 			
 		position.x = clamp(position.x + (speed * dir * delta), 50, 974)
-				
-		$AnimationPlayer.play("walking")
-		state = "walking"
+
+	if (Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right")):
+		if (isOnFloor):	
+			$AnimationPlayer.play("walking")
+			state = "walking"
 		
 	if Input.is_action_just_released("right") or Input.is_action_just_released("left"):
-		$AnimationPlayer.stop(false)
-		$Sprite.frame = 0
-		state = "idle"
-	
+		if (isOnFloor):
+			$AnimationPlayer.stop(false)
+			$Sprite.frame = 0
+			state = "idle"
+		
 	########## Jumping and falling ###########
-	if (Input.is_action_pressed("up") or Input.is_action_pressed("down")):	
+	if (Input.is_action_just_pressed("up")):	
 		dir = 0
-		if (Input.is_action_pressed("up")):
-			dir = -1
-			
-		if (Input.is_action_pressed("down")):
-			dir = 1
-			
-		position.y += speed * dir * delta	
+		dir = -1
+		inertia = -JUMPING_POWER
+		isOnFloor = false
+		$Sprite.frame = 1
+		$AnimationPlayer.stop(false)
 	
+	if (not isOnFloor):
+		position.y += inertia * delta
+		if (inertia < 100):
+			inertia += GRAVITY
 	
 	########## Greeting ##########
 	if (Input.is_action_pressed("say_hi")):
@@ -50,3 +61,7 @@ func _process(delta):
 	if Input.is_action_just_released("say_hi"):
 		$Sprite.frame = 0
 
+func _on_character_area_entered(area):
+	state = "idle"
+	$Sprite.frame = 0
+	isOnFloor = true
